d3deda4f08f41c26c1c4754be14ccf335d16c26b
Micheal Limones <michlimones@gmail.com>
2024-05-26 13:50:14 -0500

    added some styling to the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
+'use client';
+
 import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
 
 const links = [
   { url: '/', title: 'Home' },
@@ -8,9 +12,17 @@ const links = [
 ];
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div className="">
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <Link href={link.url} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+      <div className="md:hidden lg:flex w-1/3 justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -21,19 +33,41 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div className="">
-        <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="#">
+          <Image src="/instagram.png" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/facebook.png" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/github.png" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/dribbble.png" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/pinterest.png" width={24} height={24} />
+        </Link>
+      </div>
+      <div className="md:hidden">
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen(!open)}
+        >
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-black rounded"></div>
           <div className="w-10 h-1 bg-black rounded"></div>
         </button>
-        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-          {links.map((link) => (
-            <Link href={link.url} key={link.title}>
-              {link.title}
-            </Link>
-          ))}
-        </div>
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
