25ae6c8075677a511ffa9519c9b32e5c29fdb61f
Micheal Limones <michlimones@gmail.com>
2024-05-26 13:27:24 -0500

    created menu bar and menu items.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,40 @@
+import Link from 'next/link';
+
+const links = [
+  { url: '/', title: 'Home' },
+  { url: '/about', title: 'About' },
+  { url: '/portfolio', title: 'Portfolio' },
+  { url: '/contact', title: 'Contact' },
+];
+
 const Navbar = () => {
   return (
-    <div className="h-full flex items-center justify-between">
-      <div className="">1</div>
-      <div className="">2</div>
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">David Adrian </span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            Hair
+          </span>
+        </Link>
+      </div>
+      <div className="">
+        <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+        </button>
+        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+          {links.map((link) => (
+            <Link href={link.url} key={link.title}>
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
