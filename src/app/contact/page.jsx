992548c4aa4d683833c8a15263e7da25d8bdaaa2
Micheal Limones <michlimones@gmail.com>
2024-05-27 10:43:36 -0500

    added animation say hello to contact page.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,8 @@
 import { motion } from 'framer-motion';
 
 const ContactPage = () => {
+  const text = 'Say Hello';
+
   return (
     <motion.div
       className="h-full"
@@ -9,7 +11,30 @@ const ContactPage = () => {
       animate={{ y: '0%' }}
       transition={{ duration: 1 }}
     >
-      contact page
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex item-center text-center justify-center text-6xl">
+          <div className="">
+            {text.split('').map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            👋🏽
+          </div>
+        </div>
+        <div className="h-1/2 lg:h-full lg:w-1/2">
+          <span></span>
+        </div>
+      </div>
     </motion.div>
   );
 };
