ef891d92a8d52857cc50c364e374b2b73177fec5
Micheal Limones <michlimones@gmail.com>
2024-05-27 10:14:06 -0500

    added contact button at bottom of page.

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -24,7 +24,7 @@ const items = [
   {
     id: 3,
     color: 'from-violet-300 to-purple-300',
-    title: 'Project 3',
+    title: 'Project 13',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
     img: 'https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
     link: 'https://lama.dev',
@@ -83,6 +83,37 @@ const PortfolioPage = () => {
               </div>
             ))}
           </motion.div>
+        </div>
+      </div>
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-8xl">
+          Schedule a consult call to get similar look!{' '}
+        </h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: 'linear', repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Hairstylist / barber extraordinaire
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            Schedule Consult
+          </Link>
         </div>
       </div>
     </motion.div>
