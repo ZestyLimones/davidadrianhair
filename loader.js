9dcb14da1a148a96a1539961429f7c5253258a4a
Micheal Limones <michlimones@gmail.com>
2024-05-27 17:59:30 -0500

    updated loader for new s3 bucket url.

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
 export default function myImageLoader({ src, width, quality }) {
   if (src.startsWith('https://images.pexels.com')) return src;
-  return `http://davidadrianhair.s3-website.us-east-2.amazonaws.com${src}?${width}&q=${
+  return `http://davidadrianhair.com.s3-website.us-east-2.amazonaws.com${src}?${width}&q=${
     quality || 75
   }`;
 }
