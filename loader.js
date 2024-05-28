a4767c78f058d5bb959e92417fb4d002837b7409
Micheal Limones <michlimones@gmail.com>
2024-05-27 22:48:35 -0500

    updated loader image link with new url."

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
 export default function myImageLoader({ src, width, quality }) {
   if (src.startsWith('https://images.pexels.com')) return src;
-  return `http://davidadrianhair.com.s3-website.us-east-2.amazonaws.com${src}?${width}&q=${
+  return `https://s3.us-east-2.amazonaws.com/davidadrianhair.com${src}?${width}&q=${
     quality || 75
   }`;
 }
