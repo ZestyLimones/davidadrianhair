3ed3cea76de648a648930f56466298430461ec1f
Micheal Limones <michlimones@gmail.com>
2024-05-31 11:49:51 -0500

    added styling and davids contact info.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,8 +7,6 @@ const ContactPage = () => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
 
-  const text = 'Say Hello';
-
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -41,56 +39,76 @@ const ContactPage = () => {
       animate={{ y: '0%' }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
-          <div className="">
-            <div className=" bg-stone-200 rounded mt-10 w-600px h-600px">
-              <iframe
-                // className="w-full h-full"
-                title="Hair Consult Call with David"
-                src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ17jy5xLqHcU3R1NBwD5mk_9ZSl-vBZl0isqEr5MKOHCxIym02eD6xpPH8vSMk2DUxTKB2xEnkT?gv=true"
-                width="500"
-                height="600"
-              ></iframe>
+      <div className="h-auto w-full md:h-screen text-xl items-center p-2 md:p-10 justify-center items-center">
+        <h1 className="font-bold text-4xl text-yellow-500 rounded-md underline decoration-yellow-200 text-center bg-stone-950/30 p-2">
+          Get in Touch
+        </h1>
+        <div className="h-full flex flex-col lg:flex-row">
+          <div className=" h-full lg:w-1/2 flex flex-col items-center text-6xl">
+            <h1 className="font-bold text-4xl text-yellow-500 rounded-md underline decoration-yellow-200 text-center bg-stone-950/30 p-2">
+              Schedule a Call
+            </h1>
+            <div className="">
+              <div className=" h-100 bg-stone-200 rounded w-600px h-600px">
+                <iframe
+                  title="Hair Consult Call with David"
+                  src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ17jy5xLqHcU3R1NBwD5mk_9ZSl-vBZl0isqEr5MKOHCxIym02eD6xpPH8vSMk2DUxTKB2xEnkT?gv=true"
+                  width="500"
+                  height="700"
+                  className="bg-stone-200 rounded-md ring-2 ring-yellow-200"
+                ></iframe>
+              </div>
             </div>
           </div>
-        </div>
-        <form
-          onSubmit={sendEmail}
-          ref={form}
-          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
-        >
-          <span>Hey David,</span>
-          <textarea
-            rows={4}
-            placeholder="enter message you would like to send"
-            className="bg-transparent border-b-2 border-b-stone-950 outline-none resize-none content-end"
-            name="user_message"
-            required
-          ></textarea>
-          <span>Thank you,</span>
-          <input
-            type="email"
-            placeholder="enter email address"
-            className="bg-transparent border-b-2 border-b-stone-950 outline-none"
-            name="user_email"
-            required
-          />
+          <div className="h-full lg:w-1/2 flex flex-col items-center text-6xl">
+            <h1 className="font-bold text-4xl text-yellow-500 rounded-md underline decoration-yellow-200 text-center bg-stone-950/30 p-2">
+              Send me an email
+            </h1>
+            <form
+              onSubmit={sendEmail}
+              ref={form}
+              className="h-3/5 w-full bg-red-500 rounded-xl text-xl flex flex-col gap-8 justify-center p-8 bg-stone-200 text-stone-950 rounded-md ring-2 ring-yellow-200"
+            >
+              <span>Hey David,</span>
+              <textarea
+                rows={4}
+                placeholder="enter message you would like to send"
+                className="bg-transparent border-b-2 border-b-stone-950 outline-none resize-none content-end"
+                name="user_message"
+                required
+              ></textarea>
+              <span>Thank you,</span>
+              <input
+                type="text"
+                placeholder="your name"
+                className="bg-transparent border-b-2 border-b-stone-950 outline-none"
+                name="user_name"
+                required
+              />
+              <input
+                type="email"
+                placeholder="enter email address"
+                className="bg-transparent border-b-2 border-b-stone-950 outline-none"
+                name="user_email"
+                required
+              />
 
-          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
-            Send
-          </button>
-          {success && (
-            <span className="text-green-600 font-semibold">
-              Your message was sent to David.
-            </span>
-          )}
-          {error && (
-            <span className="text-red-600 font-semibold">
-              You message was not sent.
-            </span>
-          )}
-        </form>
+              <button className="bg-yellow-500 ring-2 ring-yellow-200 rounded font-semibold text-stone-950 p-4">
+                Send
+              </button>
+              {success && (
+                <span className="text-yellow-500 font-semibold">
+                  Your message was sent to David. 😊
+                </span>
+              )}
+              {error && (
+                <span className="text-red-600 font-semibold">
+                  You message was not sent.
+                </span>
+              )}
+            </form>
+          </div>
+        </div>
       </div>
     </motion.div>
   );
