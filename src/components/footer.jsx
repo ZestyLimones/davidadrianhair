38c7793efaf463c6de2666b0e68ba65d7f7a518e
Micheal Limones <michlimones@gmail.com>
2024-06-01 19:24:42 -0500

    added github link to footer.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import FooterLink from './FooterLink';
+import Link from 'next/link';
 
 const navLinks = [
   { url: '/', title: 'Home' },
@@ -27,30 +28,39 @@ const contactLinks = [
 
 const Footer = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-around  bg-stone-950 text-stone-200 p-5">
-      <div className="flex flex-col pb-8">
-        <h2 className="font-semibold underline decoration-double">
-          Important Links
-        </h2>
-        {navLinks.map((link) => (
-          <FooterLink link={link} key={link.title} />
-        ))}
+    <div className="flex flex-col bg-stone-950 text-stone-200 p-5">
+      <div className="flex flex-col md:flex-row justify-around  bg-stone-950 text-stone-200 p-5">
+        <div className="flex flex-col pb-8">
+          <h2 className="font-semibold underline decoration-double">
+            Important Links
+          </h2>
+          {navLinks.map((link) => (
+            <FooterLink link={link} key={link.title} />
+          ))}
+        </div>
+        <div className="flex flex-col pb-8">
+          <h2 className="font-semibold underline decoration-double">
+            Contact Info
+          </h2>
+          {contactLinks.map((link) => (
+            <FooterLink link={link} key={link.title} />
+          ))}
+        </div>
+        <div className="flex flex-col">
+          <h2 className="font-semibold underline decoration-double">
+            Social Links
+          </h2>
+          {socialLinks.map((link) => (
+            <FooterLink link={link} key={link.title} target="_blank" />
+          ))}
+        </div>
       </div>
-      <div className="flex flex-col pb-8">
-        <h2 className="font-semibold underline decoration-double">
-          Contact Info
-        </h2>
-        {contactLinks.map((link) => (
-          <FooterLink link={link} key={link.title} />
-        ))}
-      </div>
-      <div className="flex flex-col">
-        <h2 className="font-semibold underline decoration-double">
-          Social Links
-        </h2>
-        {socialLinks.map((link) => (
-          <FooterLink link={link} key={link.title} target="_blank" />
-        ))}
+      <div className="text-center">
+        <Link href="https://www.github.com/zestylimones" target="_blank">
+          <p className="text-xs text-stone-200">
+            © 2024 ZestyLimones. All rights reserved.
+          </p>
+        </Link>
       </div>
     </div>
   );
